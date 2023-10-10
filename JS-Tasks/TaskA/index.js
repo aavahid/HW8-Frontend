5d038b8cef25c379f719d81518a535088f268a45
Vahid Aghazada <135530864+aavahid@users.noreply.github.com>
2023-10-10 00:57:38 +0000

    Function added

--- JS-Tasks/TaskA/index.js
+++ JS-Tasks/TaskA/index.js
@@ -4,3 +4,13 @@ console.log(reverseArray.reverse());
 // Girilen arrayin reverse edilmis versiyasini return eden funksiya hazirlayin.
 // Example:
 // console.log(reverseArray([1, 2, 3, 4, 5])) -> [5, 4, 3, 2, 1]
+
+// function reverseArray(arr) {
+//   const reversedArray = [];
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     reversedArray.push(arr[i]);
+//   }
+//   return reversedArray;
+// }
+// console.log(reverseArray([1, 2, 3, 4, 5]));
+// console.log(reverseArray(["a", "b", "c", "d", "e"]));
